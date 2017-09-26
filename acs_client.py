a708c143a4387dda41e206af50e1c058f9110c32
Isac Cavalcante <isaccavalcante@alu.ufc.br>
2017-09-25 21:41:37 -0300

    Update acs_client.py

--- acs_client.py
+++ acs_client.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 #-*-coding: utf-8-*
 # thanks God
-# script for sending a single arp packet, with 'op' field modified
-# added option: who-is-acs
 import logging
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR) # supressing warnings
-from scapy.all import ARP, send
+from scapy.all import ARP, Ether, send
 
-# When a new node joins the network, it broadcasts a `Who is ACS` request
+# When a new node joins the network, it broadcasts a `Who is ACS` request.
+def sendBroadcast():
+    a = ARP()
+    a.fields_desc[4].s2i['who-is-acs'] = 28
+    a.fields_desc[4].i2s[28] = 'who-is-acs'
+    a.op = 'who-is-acs'
+    a.hwdst = "ff:ff:ff:ff:ff:ff"
+    send(a)
+
+sendBroadcast()
